790397ed6bb9507bf87d34f10fc83db05fc2318f
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2025-01-01 07:50:45 +0200

    add some animations to splashView

--- lib/features/splash/widgets/splash_view_body.dart
+++ lib/features/splash/widgets/splash_view_body.dart
@@ -3,8 +3,31 @@ import 'package:day_task/features/splash/widgets/splash_view_logo.dart';
 import 'package:day_task/features/splash/widgets/splash_view_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+  @override
+  void initState() {
+    initSlidingAnimation();
+    _controller.forward();
+    super.initState();
+  }
+
+ 
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +48,32 @@ class SplashViewBody extends StatelessWidget {
               color: Colors.white,
               child: Image.asset(Assets.splashImage),
             ),
-            const SplashViewText(),
+            AnimatedBuilder(
+              animation: _animation,
+              builder: (context, child) {
+                return SlideTransition(
+                  position: _animation,
+                  child: const SplashViewText(),
+                );
+              },
+            )
           ],
         ),
       ),
     );
+
+  }
+   void initSlidingAnimation() {
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _animation = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: const Offset(0, 0),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
   }
 }
