c53ab62ca1f686e37909248a33caed82a7483777
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2025-01-06 14:29:40 +0200

    create task model

---
+++ lib/core/models/task_model.dart
@@ -0,0 +1,10 @@
+class TaskModel {
+  final String title;
+  final String description;
+  final String date;
+  final String time;
+
+  TaskModel(this.title, this.description, this.date, this.time);
+  // final DateTime date;
+  // final TimeOfDay time;
+}
