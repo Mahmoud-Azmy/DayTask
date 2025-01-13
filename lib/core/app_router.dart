d4d47944f10a4221603e9e6ce2dcf363b6effb2e
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2025-01-13 13:36:37 +0200

    navigate to edit task view

--- lib/core/app_router.dart
+++ lib/core/app_router.dart
@@ -1,4 +1,6 @@
+import 'package:day_task/core/models/task_model.dart';
 import 'package:day_task/features/home/presentation/views/create_new_task_view.dart';
+import 'package:day_task/features/home/presentation/views/edit_task_view.dart';
 import 'package:day_task/features/home/presentation/views/home_view.dart';
 import 'package:day_task/features/splash/splash_view.dart';
 import 'package:go_router/go_router.dart';
@@ -6,6 +8,7 @@ import 'package:go_router/go_router.dart';
 abstract class AppRouter {
   static const homeView = '/homeView';
   static const createNewTaskView = '/createNewTaskView';
+  static const editTaskView = '/editTaskView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -19,6 +22,12 @@ abstract class AppRouter {
       GoRoute(
         path: createNewTaskView,
         builder: (context, state) => const CreateNewTaskView(),
+      ),
+      GoRoute(
+        path: editTaskView,
+        builder: (context, state) => EditTaskView(
+          taskModel: state.extra as TaskModel,
+        ),
       ),
     ],
   );
--- lib/features/home/presentation/views/widgets/tasks_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/tasks_list_view_item.dart
@@ -1,9 +1,11 @@
 import 'package:day_task/consts.dart';
+import 'package:day_task/core/app_router.dart';
 import 'package:day_task/core/models/task_model.dart';
 import 'package:day_task/core/styles.dart';
 import 'package:day_task/features/home/presentation/manager/get_tasks_cubit/get_tasks_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class TasksListViewItem extends StatelessWidget {
   const TasksListViewItem({super.key, required this.taskModel});
@@ -11,48 +13,57 @@ class TasksListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      decoration: BoxDecoration(
-        color: kThirdColor,
-        borderRadius: BorderRadius.circular(6),
-      ),
-      child: Row(
-        children: [
-          Container(
-            width: 8,
-            color: kSeconedColor,
-          ),
-          const SizedBox(width: 20),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(taskModel.title,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400,
-                  )),
-              Text('18:30',
-                  style: Styles.textStyle12.copyWith(
-                      fontWeight: FontWeight.w500, color: Colors.grey)),
-            ],
-          ),
-          const Spacer(),
-          IconButton(
-            onPressed: () {
-              taskModel.delete();
-              BlocProvider.of<GetTasksCubit>(context).getTasks();
-            },
-            icon: const Icon(
-              Icons.delete,
-              size: 32,
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.editTaskView, extra: taskModel);
+      },
+      child: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          color: kThirdColor,
+          borderRadius: BorderRadius.circular(6),
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 8,
+              color: kSeconedColor,
             ),
-          ),
-          const SizedBox(
-            width: 10,
-          )
-        ],
+            const SizedBox(width: 20),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Text(taskModel.title,
+                      maxLines: 1,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                      )),
+                ),
+                Text('18:30',
+                    style: Styles.textStyle12.copyWith(
+                        fontWeight: FontWeight.w500, color: Colors.grey)),
+              ],
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {
+                taskModel.delete();
+                BlocProvider.of<GetTasksCubit>(context).getTasks();
+              },
+              icon: const Icon(
+                Icons.delete,
+                size: 32,
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            )
+          ],
+        ),
       ),
     );
   }
